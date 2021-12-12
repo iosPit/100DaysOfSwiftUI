568db186c19cfbce3baeb3bda49004afd7f7691d
Pitambar Dayal <pitambardayal@Pitambars-MacBook-Air.local>
2021-12-12 12:34:55 -0500

    add BetterRest project

---
+++ 04_BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Pitambar Dayal on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
