c5923715230b6a98066673145b937698b77f88d6
Pitambar Dayal <pitambardayal@Pitambars-MacBook-Air.local>
2021-12-21 11:13:48 -0500

    adding Multiplication tables project

---
+++ 07_MultiplicationTables/MultiplicationTables/MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Pitambar Dayal on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
