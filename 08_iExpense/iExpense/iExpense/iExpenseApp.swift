091833f2f1c1cbf621ce6e5a6cb085f1f8904c62
Pitambar Dayal <92935672+iosPit@users.noreply.github.com>
2021-12-26 20:07:18 -0500

    adding iExpense Files

---
+++ 08_iExpense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Pitambar Dayal on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
